44bcaa0d8e35e598d685fbdf947bd40b0124a06b
Pras Velagapudi <psigen@gmail.com>
2014-01-05 07:31:45 -0500

    Updated flexure header to match test.cpp more closely.

--- src/flexure.h
+++ src/flexure.h
@@ -1,3 +1,42 @@
+#ifndef FLEXURE_H
+#define FLEXURE_H
+
+#include <string>
+
+namespace Flexure
+{
+struct Undefined;
+
+template <typename T = Undefined>
+class Flexure
+{
+ public:
+  Flexure(const string& name = "");
+  virtual ~Flexure();
+
+  // Data accessor methods
+  Flexure<T>& operator=(const T&);  
+  operator T();
+
+  // Runtime casting accessor methods
+  template <typename T2> Flexure<T2> operator=(const T2& value);
+  template <typename T2> operator T2(); 
+
+  // Hierarchy accessor methods
+  Flexure<Undefined> operator[](const string& name);
+  template <typename T2> Flexure<T2> Get(const string& name); 
+  
+  // Meta-accessor methods
+  void Observe(void (*function)(const T& old_val, const T& new_val));
+  void Unobserve(void (*function)(const T& old_val, const T& new_val));
+  
+ private:
+  Flexure::Entry entry_;
+};
+}
+
+#endif FLEXURE_H
+
 template<typename T>
 class Flexure
 {
