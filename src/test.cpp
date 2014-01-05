fb607c311652e40ed694b19d80eb31cd9c2abac5
Joydeep Biswas <Joydeep@iBlue.local>
2014-01-04 22:47:38 -0500

    More hackery

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <map>
 #include <string>
+#include "type_enum.h"
 
+using std::map;
 using std::string;
 
 template <typename T>
@@ -18,20 +21,40 @@ constexpr int TypeName<double>(const double& x) {
   return 2;
 }
 
+struct Undefined {
+};
+
 class InferenceFlexure;
 
-template <typename T = void*>
+template <typename T = Undefined>
 class Flexure {
  public:
   Flexure(const string& name) : name_(name) {
   };
   
-  Flexure<T>& operator=(const T&) {}
-  InferenceFlexure operator[](const string& name);
-
+  Flexure<T>& operator=(const T&) {
+    return (*this);
+  }
+  Flexure<Undefined> operator[](const string& name);
+ 
+  template <typename T_Other>
+  Flexure<T_Other> operator=(const T_Other& value);
+  
   T* x_;
   const string name_;
 };
+
+template <> template <typename T_Other>
+Flexure<T_Other> Flexure<Undefined>::operator=(const T_Other& value) {
+   return (Flexure<T_Other>(name_));
+}
+
+/*
+template <typename T> template <typename T_Other = T>
+Flexure<T_Other> Flexure<T>::operator=(const T_Other& value) {
+   return (Flexure<T>(name_));
+}
+*/
 
 class InferenceFlexure {
   public:
@@ -45,23 +68,67 @@ class InferenceFlexure {
 };
 
 template <typename T>
-InferenceFlexure Flexure<T>::operator[](const string& name)
+Flexure<Undefined> Flexure<T>::operator[](const string& name)
 {
-  return InferenceFlexure(name);
+  return Flexure<Undefined>(name);
 }
+
+class Memory {
+  Memory();
+  void* pointer();
+  ~Memory();
+};
+
+// Would help to have this be compatible with std::allocator for transparent use
+// with std::map for schema.
+class HeapAllocator {
+  void Init(Memory* memory) {}
+  void* Alloc(size_t size);
+  void Free(void* ptr);
+};
+
+class Entry {
+
+ protected:
+  FlexureTypes type_;
+  size_t ref_count_;
+ public:
+  void* pointer_;
+};
+
+class FlexureDataStore {
+ public:
+  FlexureDataStore();
+  Entry& Get(const string& fq_name);
+
+ protected:
+  map<string, Entry> schema_;
+  HeapAllocator heap_alloc_;
+};
 
 int main() {
   int a = 3;
   double b = 5.0;
   const int type_a = TypeName(a);
   const int type_b = TypeName(b);
-  printf("b = %d\n", type_b);
+  printf("a : %d\n", type_a);
+  printf("b : %d\n", type_b);
   Flexure<> f("/");
+  auto apple = f["apple"];
+  apple = 3;
+  // The following works at compile time, but should fail at run time:
+  apple = "String";
+
   auto bar = (f["apple"] = 2);
   f["pear"] = "String";
   // The following is valid:
   bar = 3;
-
+  
+  auto foo = bar["foo"];
+  foo = 2;
+  foo = "blah";
+  
+  
   // The following will throw a compile error:
   // bar = "string";
   return 0;
---
+++ src/type_enum.h
@@ -0,0 +1,10 @@
+#ifndef TYPE_ENUM_H_
+#define TYPE_ENUM_H_
+
+enum FlexureTypes {
+  UNKNOWN = 0,
+  INT32,
+  FLOAT32,
+};
+
+#endif  // TYPE_ENUM_H_
